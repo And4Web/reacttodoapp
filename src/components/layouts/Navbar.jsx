a7a241c4e2dd575e0a0717fa850397cef1096e1d
And4Web <anands2807@gmail.com>
2022-02-23 14:16:50 -0800

    bootstrap form added

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -4,14 +4,14 @@ import NavItem from "./NavItem";
 
 function Navbar() {
   return (
-    <div>
+    <>
       <nav className="navbar navbar-light bg-light d-flex justify-content-center">
         <Link to="/" className="navbar-brand">
           <h3>ToDo App</h3>
         </Link>
         <NavItem />
       </nav>
-    </div>
+    </>
   );
 }
 
