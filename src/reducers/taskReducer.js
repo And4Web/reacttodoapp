1344f9b29e03de73d77469d93bc24dee97663206
And4Web <anands2807@gmail.com>
2022-02-25 09:43:35 -0800

    taskReducer created

---
+++ src/reducers/taskReducer.js
@@ -0,0 +1,18 @@
+// import AddTask from "../actions/taskActions";
+
+const taskReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "ADD_TASK": {
+      console.log("a new task is added.");
+      return state;
+    }
+    case "ADD_TASK_ERR": {
+      console.log("an error occurred.");
+      return state;
+    }
+    default:
+      return state;
+  }
+};
+
+export default taskReducer;
